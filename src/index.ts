51d9a05364f70a52bf3426fa3f5ca1b3bbbd3f6f
Stefano Ruth <6178983+stefanoruth@users.noreply.github.com>
2023-08-09 15:40:47 +0200

    Remove test from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,1 @@
-import { envParser } from './EnvParser'
-
 export { envParser } from './EnvParser'
-
-const args = envParser({ port: { type: 'const', options: ['foo'] }, portB: { type: 'const', options: ['as', 'prod'] } })
-
-args.portB === 'prod'
